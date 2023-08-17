e75b11264508131bb3f26e0fceefb736e8e86848
Thomas Leiber <leiber@kk-software.de>
2023-08-17 15:19:03 +0200

    added debug output

--- sys/apps/lyra_backlight/__init__.py
+++ sys/apps/lyra_backlight/__init__.py
@@ -10,6 +10,7 @@ import math
 class Blacklight(Application):
     def __init__(self, app_ctx: ApplicationContext) -> None:
         super().__init__(app_ctx)
+
         self.v_x = 0.0
         self.v_y = 0.0
         self.v_z = 0.0
@@ -22,9 +23,31 @@ class Blacklight(Application):
     def draw(self, ctx: Context) -> None:
         ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
         ctx.rgb(255, 0, 255)
+        ctx.font = ctx.get_font_name(5)
 
-        ctx.move_to(0, 0)
-        ctx.save()
+        ctx.move_to(-100, -35)
+        if self.v_x < 0:
+            ctx.rgb(255, 0, 255)
+            ctx.text("X=" + str(self.v_x))
+        else:
+            ctx.rgb(0, 255, 0)
+            ctx.text("X=+" + str(self.v_x))
+
+        ctx.move_to(-100, 0)
+        if self.v_y < 0:
+            ctx.rgb(255, 0, 255)
+            ctx.text("Y=" + str(self.v_y))
+        else:
+            ctx.rgb(0, 255, 0)
+            ctx.text("Y=+" + str(self.v_y))
+
+        ctx.move_to(-100, 35)
+        if self.v_z < 0:
+            ctx.rgb(255, 0, 255)
+            ctx.text("Z=" + str(self.v_z))
+        else:
+            ctx.rgb(0, 255, 0)
+            ctx.text("Z=+" + str(self.v_z))
 
     def on_exit(self) -> None:
         for led_index in range(9, 33):
