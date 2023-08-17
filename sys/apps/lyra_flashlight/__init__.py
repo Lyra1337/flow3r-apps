1db50d2c5e7b826aa239535b5ad4e5e0c278532d
Thomas Leiber <leiber@kk-software.de>
2023-08-17 15:16:51 +0200

    reset all leds, changed leds to reduce light leakage

--- sys/apps/lyra_flashlight/__init__.py
+++ sys/apps/lyra_flashlight/__init__.py
@@ -11,10 +11,13 @@ class Flashlight(Application):
     def __init__(self, app_ctx: ApplicationContext) -> None:
         super().__init__(app_ctx)
 
+        for led_index in range(0, 41):
+            leds.set_rgb(led_index, 0, 0, 0)
+
     def think(self, ins: InputState, delta_ms: int) -> None:
         leds.set_brightness(255)
 
-        for led_index in range(9, 33):
+        for led_index in range(9, 32):
             leds.set_rgb(led_index, 255, 255, 255)
 
         leds.update()
@@ -27,7 +30,7 @@ class Flashlight(Application):
         ctx.save()
 
     def on_exit(self) -> None:
-        for led_index in range(9, 33):
+        for led_index in range(9, 32):
             leds.set_rgb(led_index, 0, 0, 0)
 
         leds.set_brightness(255)
